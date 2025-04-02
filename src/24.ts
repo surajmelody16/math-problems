1eab3b9c58551585e14129ac366af426dd4c7aa5
surajmelody16 <surajmelody16@gmail.com>
2025-04-02 06:26:26 +0000

    Add 24.ts

---
+++ src/24.ts
@@ -0,0 +1,17 @@
+import { createTheme } from '@material-ui/core';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#2e7d3a',
+    },
+    secondary: {
+      main: '#1f8ad5',
+    },
+    error: {
+      main: '#ff4961',
+    },
+  },
+});
+
+export default theme;
