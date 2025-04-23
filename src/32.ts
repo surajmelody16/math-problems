f0df548ebad8031f417a311c5d0c1bdcdba0e43f
surajmelody16 <surajmelody16@gmail.com>
2025-04-23 11:31:13 +0000

    Add 32.ts

---
+++ src/32.ts
@@ -0,0 +1,10 @@
+// Problem statement: Write a program to calculate the sum of two numbers.
+// Example input: 
+// 2 + 3 = 5
+// Output: 5
+
+function addTwoNumbers(num1: number, num2: number): number {
+    return num1 + num2;
+}
+
+console.log(addTwoNumbers(2, 3)); // Expected output: 5
